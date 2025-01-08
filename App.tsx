806b1ce6fa5eb84bd90f35dea811270592688894
Mario Dias <mario.dias@ufpi.edu.br>
2025-01-08 14:19:04 -0300

    uploading files in a bare project

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import { OneSignal } from 'react-native-onesignal'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("72c66226-ce43-46f5-a0bc-489aa4228f60")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
